ae6d7f06b083ab4b463f835a0a266e8c125e3486
Oleh Chui <misterchuy21@gmail.com>
2026-01-16 12:28:12 +0200

    TASK-3: [resource-service] Replace RestTemplate with OpenFeing

--- resource-service/src/main/java/chui/config/ServiceConfig.java
+++ resource-service/src/main/java/chui/config/ServiceConfig.java
@@ -2,15 +2,13 @@ package chui.config;
 
 import chui.client.MetadataApiClient;
 import chui.repository.ResourceRepository;
-import chui.service.ResourceService;
-import chui.service.Mp3MetadataExtractorService;
-import chui.service.Mp3ValidationService;
 import chui.service.CsvIdParserService;
 import chui.service.IdParserService;
-import org.springframework.beans.factory.annotation.Value;
+import chui.service.Mp3MetadataExtractorService;
+import chui.service.Mp3ValidationService;
+import chui.service.ResourceService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class ServiceConfig {
@@ -22,17 +20,8 @@ public class ServiceConfig {
                                          Mp3ValidationService mp3ValidationService,
                                          CsvIdParserService csvIdParserService,
                                          IdParserService idParserService) {
-    return new ResourceService(resourceRepository, metadataApiClient, mp3MetadataExtractorService, mp3ValidationService, csvIdParserService, idParserService);
-  }
-
-  @Bean
-  public RestTemplate restTemplate() {
-    return new RestTemplate();
-  }
-
-  @Bean
-  public MetadataApiClient metadataApiClient(RestTemplate restTemplate, @Value("${spring.song-service.url}") String songServiceUrl) {
-    return new MetadataApiClient(restTemplate, songServiceUrl);
+    return new ResourceService(resourceRepository, metadataApiClient, mp3MetadataExtractorService,
+        mp3ValidationService, csvIdParserService, idParserService);
   }
 
   @Bean
